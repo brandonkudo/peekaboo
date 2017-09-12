b26c49b899ffe42a0f9a065d6d9d0f46281286cb
brandon kudo and kyle ebersole <brandon@>
2017-09-12 10:22:15 -0700

    add buttons with color and size toggles

--- js/script.js
+++ js/script.js
@@ -1,6 +1,31 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
-    $("#initially-hidden").toggle();
-    $("#initially-showing").toggle();
+    $("#initially-hidden").slideToggle();
+    $("#initially-showing").slideToggle();
+  });
+
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#text-big").click(function() {
+    $("p").removeClass();
+    $("p#last-paragraph").addClass("big");
+  });
+
+  $("button#text-small").click(function() {
+    $("p").removeClass();
+    $("p#last-paragraph").addClass("small");
   });
 });
